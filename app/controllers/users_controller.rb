de564af635f27d09f92933373169c517d8d552ad
Jason Watkins <goushoryuken@gmail.com>
2022-04-28 10:49:08 -0700

    done, working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(allowed_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(allowed_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def allowed_params
+    params.require(:user).permit(:username,:email,:password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "users#new"
+
+  resources :users do
+
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
